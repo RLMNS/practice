722515dcccd59d2baf96538b3d1fb1e15d395aa2
Rustam <rustammslmn@gmail.com>
2023-05-24 18:10:55 +0600

    modified:   task1/untitled/out/production/untitled/Solution.class 	modified:   task1/untitled/src/Solution.java
    modified:   task1/untitled/out/production/untitled/Solution.class
    modified:   task1/untitled/src/Solution.java

--- task1/untitled/src/Solution.java
+++ task1/untitled/src/Solution.java
@@ -7,20 +7,7 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) {
-        String text = "\n" +
-                "Hong Kong CNN  — \n" +
-                "\n" +
-                "\n" +
-                "         \n" +
-                "\n" +
-                "        Even in 40. 0 a land known for (gargantuan),record-breaking infrastructure, this project is turning heads.\n                   " +
-                "At 15 miles long (24 kilometers), eight lanes wide and featuring artificial islands and an undersea tunnel, China’s $6.7 billion Shenzhen-Zhongshan Bridge is nothing if not ambitious.\n" +
-                "\n" +
-                "To much fanfare in the country’s state media, the bridge’s builders recently claimed a new world record by paving in a single day more than 243,200 square feet (22,600 square meters) of asphalt, the equivalent of more than 50 basketball courts.\n" +
-                "\n" +
-                "Yet strange as it may sound, this is not the world’s longest sea bridge. That honor belongs to its 34-mile long neighbor, the Hong Kong-Zhuhai-Macao Bridge – just 20 miles away.\n" +
-                "\n" +
-                "To some observers, the building of these giant bridges in such close proximity is testament both to China’s growing ambitions on the global stage and the problems it faces in realizing them. ";
+        String text = args[0];
 
         List<String> paragraphs = Arrays.stream(text.split("[\r\n]+")).filter(Predicate.not(String::isBlank))
                 .map(String::trim).toList();
@@ -28,7 +15,7 @@ public class Solution {
         paragraphs.forEach(paragraph -> {
             System.out.println("Paragraph: " + paragraph);
             System.out.println("Word count: " +
-                    getMatches(paragraph, "(\\b[^\\s—#$%&'()*+,./:;<=>?@\\[\\]\\^_\\{\\|\\}]+\\b)"));
+                    getMatches(paragraph, "(\\b[^\\s]+\\b)").size());
             System.out.println();
         });
     }
